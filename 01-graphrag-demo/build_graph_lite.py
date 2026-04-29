9bf2727c7c8b88eeb8bfb02a79396eac8500143a
Elizabeth <elizabethfuentes12>
2026-04-29 11:32:52 -0700

    Fix neo4j:// to bolt:// URI scheme, add update_if_exists=True to AgentCore launch

--- 01-graphrag-demo/build_graph_lite.py
+++ 01-graphrag-demo/build_graph_lite.py
@@ -17,7 +17,7 @@ from neo4j import GraphDatabase
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
 from bedrock_providers import BedrockLLM, BedrockEmbeddings
 
-NEO4J_URI = os.getenv("NEO4J_URI", "neo4j://127.0.0.1:7687")
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://127.0.0.1:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
 
--- 01-graphrag-demo/travel_agent_demo.py
+++ 01-graphrag-demo/travel_agent_demo.py
@@ -16,7 +16,7 @@ import json
 import boto3
 import numpy as np
 
-NEO4J_URI = os.getenv("NEO4J_URI", "neo4j://127.0.0.1:7687")
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://127.0.0.1:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "password")
 
